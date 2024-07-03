aff38926a86272b38372b5f73be4094a80a0443e
hayoungzzzzz <jhy901@naver.com>
2024-07-03 17:58:11 +0900

    chore: 사용하지 않는 파일 정리

---
+++ src/components/main/MainScreen.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const MainScreen = () => {
+  return <Wrapper>메인 화면 입니다.</Wrapper>;
+};
+
+const Wrapper = styled.div``;
+
+export default MainScreen;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
 /** ssr:false 하여야 내부 suspense가 정상 동작 한다. */
-export default dynamic(() => import('~/components/Page'), {
+export default dynamic(() => import('~/components/main/MainScreen'), {
   ssr: false,
 });
