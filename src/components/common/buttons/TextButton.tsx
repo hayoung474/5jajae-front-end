1d5f624d2beac529eada38f423d553261fd82dbc
hayoungzzzzz <jhy901@naver.com>
2024-07-31 18:30:44 +0900

    style: TextButton hover ,active css 추가

--- src/components/common/buttons/TextButton.tsx
+++ src/components/common/buttons/TextButton.tsx
@@ -4,10 +4,11 @@ import { ColorsType } from '~/style/theme';
 import Text from '../Text';
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement> & { textColor?: keyof ColorsType };
-const TextButton = ({ textColor, children, ...attr }: Props) => {
+const TextButton = ({ textColor, children, disabled, ...attr }: Props) => {
+  const color: keyof ColorsType = disabled ? 'cool_gray_300' : textColor ?? 'cool_gray_500';
   return (
     <Wrapper {...attr}>
-      <Text variant="label_2" weight="medium" color={textColor ?? 'cool_gray_500'}>
+      <Text variant="label_2" weight="medium" color={color}>
         {children}
       </Text>
     </Wrapper>
@@ -19,6 +20,36 @@ const Wrapper = styled.button`
   border: none;
   background-color: transparent;
   padding: 0;
+
+  position:relative;
+  &:hover {
+    &::before {
+      display:block;
+      position:absolute;
+      content:"";
+      width:100%;
+      height:100%;
+      border-radius: 4px;
+      background-color: #00000005;
+      padding:4px;
+      top:-4px;
+      left:-4px;
+    }
+  }
+  &:active {
+    &::before {
+      display:block;
+      position:absolute;
+      content:"";
+      width:100%;
+      height:100%;
+      border-radius: 4px;
+      background-color: #00000008;
+      padding:4px;
+      top:-4px;
+      left:-4px;
+    }
+  }
 `;
 
 export default TextButton;
