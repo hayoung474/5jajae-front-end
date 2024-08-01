62f93527ebc533ee60d56b86f1a87f8b00b9da87
hayoungzzzzz <jhy901@naver.com>
2024-08-01 23:51:33 +0900

    feat: 스크롤 여부 custom hook 추가

---
+++ src/hooks/useIsScrolled.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState, MutableRefObject } from 'react';
+import throttle from '~/lib/throttle';
+
+const checkIsScrolled = (targetElement: MutableRefObject<HTMLElement | null> | null): boolean => {
+  if (targetElement?.current) {
+    return targetElement.current.scrollTop > 0;
+  }
+  return window.scrollY > 0;
+};
+
+export const useIsScrolled = (targetElement: MutableRefObject<HTMLElement | null> | null = null): boolean => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(checkIsScrolled(targetElement));
+
+  useEffect(() => {
+    const listener = throttle(() => {
+      setIsScrolled(checkIsScrolled(targetElement));
+    }, 200);
+
+    const target = targetElement?.current || window;
+    target.addEventListener('scroll', listener, { passive: true });
+
+    return () => {
+      target.removeEventListener('scroll', listener);
+    };
+  }, [targetElement]);
+
+  return isScrolled;
+};
