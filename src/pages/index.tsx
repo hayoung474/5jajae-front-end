226a5f27dd66a71e0bdb9c89a07f6dc755d9cf7a
hayoungzzzzz <jhy901@naver.com>
2024-10-06 18:07:38 +0900

    feat: meta 태그 및 opengraph 정보 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
+import MainScreen from '~/components/main/MainScreen';
 
 /** ssr:false 하여야 내부 suspense가 정상 동작 한다. */
-export default dynamic(() => import('~/components/main/MainScreen'), {
-  ssr: false,
-});
+// export default dynamic(() => import('~/components/main/MainScreen'), {
+//   ssr: false,
+// });
+
+export default function Page() {
+  return <MainScreen />;
+}
